21c0c976096181c5754a68eba882f5227aab259b
Dexter245 <Dexter245@googlemail.com>
2016-07-05 19:54:15 +0200

    project setup

---
+++ desktop/src/com/dexter/triangles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dexter.triangles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dexter.triangles.Triangles;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Triangles(), config);
+	}
+}
---
+++ html/src/com/dexter/triangles/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.dexter.triangles.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.dexter.triangles.Triangles;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Triangles();
+        }
+}
